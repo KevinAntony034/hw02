b3e7acb81f465c0c5e202a54339849a3223ef2dd
kevinAntony034 <kevinag034@gmail.com>
2024-11-10 21:14:53 +0100

    Task 4

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,32 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
-
+from math_quiz import Limitfunc, Operatorfunc, Mathfunc
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_Limitfunc(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Limitfunc(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
-
-    def test_function_B(self):
-        # TODO
-        pass
-
-    def test_function_C(self):
-            test_cases = [
+    def test_Operatorfunc(self): 
+        valid = ("+  -  *")
+        for _ in range(1000):
+                operator = Operatorfunc()
+                self.assertIn(operator,valid)
+        pass  
+    def test_Mathfunc(self):
+            test_case = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (6, 1, '-', '6 - 1', 5),
+                (3, 3, '*', '3 * 3', 9),
+                (4, 4, '+', '4 + 4', 8),
+                (9, 5, '-', '9 - 5', 4),         
             ]
-
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
-
+            for number1, number2, operator, expected_problem, expected_answer in test_case:
+                prob, answer = Mathfunc(number1, number2, operator)
+                self.assertEqual(prob, expected_problem)   
+                self.assertEqual(answer, expected_answer)
+            pass
 if __name__ == "__main__":
     unittest.main()
